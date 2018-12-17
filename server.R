30aaad1dc8da775cd38c69a88049d9996b2090ea
baizura <baizura.mohd@gmail.com>
2018-12-18 07:28:20 +0800

    Add files via upload

--- server.R
+++ server.R
@@ -9,191 +9,236 @@ library(plotly)
 
 server <- function(input, output) {
   
-  
-  #============================
-  #     ValueBox
-  #============================
-  output$TP <- renderText({  
-    ## Total Participant (TP)
-    TP <- read.csv("./Data/summaryData.csv")
-    TP <- TP %>% filter(AttrVar=="TP") %>% select(TotalVal)
-    
-    
-    ## convert to integer and format it to have thousand seperator
-    TP <- formatC(as.integer(TP), format="f", big.mark=",", digits=0)
-  })
-  
-  output$TV <- renderText({    
-    ## Total variables (TV)
-    TV <- read.csv("./Data/summaryData.csv")
-    TV <- TV %>% filter(AttrVar=="TV") %>% select(TotalVal)
-    ## convert to integer and format it to have thousand seperator
-    TV <- formatC(as.integer(TV), format="f", big.mark=",", digits=0)
-  })
-  
-  
-  output$TC <- renderText({  
-    ## Total Country (TC)
-    TC <- read.csv("./Data/summaryData.csv")
-    TC <- TC %>% filter(AttrVar=="TC") %>% select(TotalVal)
-    ## convert to integer and format it to have thousand seperator
-    TC <- formatC(as.integer(TC), format="f", big.mark=",", digits=0)
-  })
-  
-  
-  output$ATE <- renderText({  
-    ## Average time elapse(ATE) in seconds
-    ATE <- read.csv("./Data/summaryData.csv")
-    ATE <- ATE %>% filter(AttrVar=="ATE") %>% select(TotalVal)
-    
-    ## convert to integer and format it to have thousand seperator
-    ATE <- formatC(as.integer(ATE), format="f", big.mark=",", digits=2)
-  })
-  #=============================
-  
-  output$CPDMap <- renderLeaflet({
-    
+  observe({
+    intro <- read.csv("./Data/summaryData.csv")
     CPD <- read.csv("./Data/countryData.csv")
-    
-    #joinCountryData2Map to plot the country base on joinCode
-    SPDF <- joinCountryData2Map( CPD
-                                 ,joinCode = "ISO2"
-                                 ,nameJoinColumn = "Country")
-    
-    # spdf is a sp::SpatialPolygonsDataFrame
-    qpal <- colorNumeric(rev(viridis::plasma(15)),
-                         SPDF$Total, n=15)
-    
-    CPDMap <- leaflet(SPDF, options =
-                        leafletOptions(attributionControl = FALSE, minzoom=1.5)) %>%
-      addPolygons(
-        label=~stringr::str_c(
-          NAME, ' ',
-          formatC(Total, big.mark = ',', format='d')),
-        labelOptions= labelOptions(direction = 'auto'),
-        weight=1,color='#333333', opacity=1,
-        fillColor = ~qpal(Total), fillOpacity = 1,
-        highlightOptions = highlightOptions(
-          color='#000000', weight = 2,
-          bringToFront = TRUE, sendToBack = TRUE)
-      ) %>%
-      addLegend(
-        "topright", pal = qpal, values = ~Total,
-        title = htmltools::HTML("Participant Density"),
-        opacity = 1 )
-    
-  })
-  
-  #==========================
-
-  output$osriResult <- renderPlot({
-    OSRI_summary <- read.csv("./Data/OSRI_summary.csv")
-    plot(OSRI_summary[c(3,2,1)],
-         pch = 19,
-         cex = .7,
-         bg = par("bg"),
-         col = c("purple","green", "tomato"))
-    
-  })
-  
-  output$mascAndro <- renderPlot({
-    OSRI_summary <- read.csv("./Data/OSRI_summary.csv")
-        plot(OSRI_summary[c(2,1)],  # x = Androgyny y = Masculine
-         pch = c(19, 17),
-         cex = .7,
-         col = c("purple","green"))
-    
-  })
-  output$femAndro <- renderPlot({
-    OSRI_summary <- read.csv("./Data/OSRI_summary.csv")
-    plot(OSRI_summary[-1], # x = Androgyny y = Feminine
-         pch = c(19, 17),
-         cex = .7,
-         col = c("purple","green"))
-    
-    
-    
-  })
-  
-  output$femMasc <- renderPlot({
-    OSRI_summary <- read.csv("./Data/OSRI_summary.csv")
-    plot(OSRI_summary[c(1,3)],
-         type = 'p',
-         pch = 19,
-         cex = .7,
-         col = c("purple", "green"))
-    
-  })
-  
-  
-  output$volcanoPlot <- renderPlotly({
+    by_gender <- read.csv("./Data/genderData.csv")
     OSRI_summary <- read.csv("./Data/OSRI_summary.csv")
     osriDF_new <- read.csv("./Data/osriDF_new.csv")
-    plot_ly (osriDF_new,
-             type = 'surface',
-             z = ~volcano )
-    
-    v = plot_ly (OSRI_summary,
-                 type = 'surface',
-                 z = ~volcano ) %>% 
-      layout(
-             xaxis = list(showgrid = FALSE),
-             yaxis = list(showgrid = FALSE))
-    
-    
-  })
-  
-  output$bublePlot <- renderPlotly({
     by_country <- read.csv("./Data/by_country.csv")
-    library(plotly)
-    
-    vline <- function(x = 0, color = "black") {
-      list(
-        type = "line", 
-        y0 = 0, 
-        y1 = 1, 
-        yref = "paper",
-        x0 = x, 
-        x1 = x, 
-        line = list(color = color)
-      )
-    }
-    
-    hline <- function(y = 0, color = "black") {
-      list(
-        type = "line", 
-        x0 = 0, 
-        x1 = 1, 
-        xref = "paper",
-        y0 = y, 
-        y1 = y, 
-        line = list(color = color)
-      )
-    }
-    
-    # top 20 countries
-    plot_ly(by_country, x = ~mean_masc, y = ~mean_fem, text = ~IP, type = 'scatter', 
-                      mode = 'markers', size = ~count, color = ~IP, colors = 'Paired',
-                      #Choosing the range of the bubbles' sizes:
-                      sizes = c(30, 90),
-                      marker = list(opacity = 0.5, sizemode = 'diameter')) %>%
-      layout(xaxis = list(showgrid = T),
-             yaxis = list(showgrid = T),
-             shapes = list(vline(3), hline(3)),
-             showlegend = T)
-  })
-  
-  
-  output$twoDhist <- renderPlotly({
     result <- read.csv("./Data/result.csv")
-    OSRI_summary <- read.csv("./Data/OSRI_summary.csv")
-    count <- with(result, table(Fem_Score, Masc_Score))
-    plot_ly(OSRI_summary, x = ~Masculine, y = ~Feminine, z = ~count) %>%
-      layout(xaxis = list(showgrid = FALSE),
-             yaxis = list(showgrid = FALSE)) %>% 
-      add_histogram2d()
+    
+    #============================
+    #     ValueBox
+    #============================
+    output$TP <- renderText({  
+      ## Total Participant (TP)
+      TP <- intro %>% filter(AttrVar=="TP") %>% select(TotalVal)
+      
+      ## convert to integer and format it to have thousand separator
+      TP <- formatC(as.integer(TP), format="f", big.mark=",", digits=0)
+    })
+    
+    output$TV <- renderText({    
+      ## Total variables (TV)
+      TV <- intro %>% filter(AttrVar=="TV") %>% select(TotalVal)
+      
+      ## convert to integer and format it to have thousand separator
+      TV <- formatC(as.integer(TV), format="f", big.mark=",", digits=0)
+    })
+    
+    output$TC <- renderText({  
+      ## Total Country (TC)
+      TC <- intro %>% filter(AttrVar=="TC") %>% select(TotalVal)
+      
+      ## convert to integer and format it to have thousand separator
+      TC <- formatC(as.integer(TC), format="f", big.mark=",", digits=0)
+    })
     
     
+    output$ATE <- renderText({  
+      ## Average time elapse(ATE) in seconds
+      ATE <- intro %>% filter(AttrVar=="ATE") %>% select(TotalVal)
+      
+      ## convert to integer and format it to have thousand seperator
+      ATE <- formatC(as.integer(ATE), format="f", big.mark=",", digits=2)
+    })
+    
+    #=============================
+    
+    output$CPDMap <- renderLeaflet({
+      
+      #joinCountryData2Map to plot the country base on joinCode
+      SPDF <- joinCountryData2Map( CPD
+                                   ,joinCode = "ISO2"
+                                   ,nameJoinColumn = "Country")
+      
+      # spdf is a sp::SpatialPolygonsDataFrame
+      qpal <- colorNumeric(rev(viridis::plasma(15)),
+                           SPDF$Total, n=15)
+      
+      CPDMap <- leaflet(SPDF, options = leafletOptions(attributionControl = FALSE, minzoom=1.5)) %>%
+        addPolygons(
+          label=~stringr::str_c(
+            NAME, ' ',
+            formatC(Total, big.mark = ',', format='d')),
+          labelOptions= labelOptions(direction = 'auto'),
+          weight=1,color='#333333', opacity=1,
+          fillColor = ~qpal(Total), fillOpacity = 1,
+          highlightOptions = highlightOptions(
+            color='#000000', weight = 2,
+            bringToFront = TRUE, sendToBack = TRUE)
+        ) %>%
+        addLegend(
+          "topright", pal = qpal, values = ~Total,
+          title = htmltools::HTML("Total Participant"),
+          opacity = 1 )
+      
+    })
+    
+    #==========================
+    
+    output$osriResult <- renderPlot({
+      plot(OSRI_summary[c(3,2,1)],
+           pch = 19,
+           cex = .7,
+           bg = par("bg"),
+           col = c("purple","green", "tomato"))
+      
+    })
+    
+    output$mascAndro <- renderPlot({
+      plot(OSRI_summary[c(2,1)],  # x = Androgyny y = Masculine
+           pch = c(19, 17),
+           cex = .7,
+           col = c("purple","green"))
+      
+    })
+    output$femAndro <- renderPlot({
+      plot(OSRI_summary[-1], # x = Androgyny y = Feminine
+           pch = c(19, 17),
+           cex = .7,
+           col = c("purple","green"))
+      
+      
+      
+    })
+    
+    output$femMasc <- renderPlot({
+      plot(OSRI_summary[c(1,3)],
+           type = 'p',
+           pch = 19,
+           cex = .7,
+           col = c("purple", "green"))
+      
+    })
+    
+    
+    output$volcanoPlot <- renderPlotly({
+      plot_ly (osriDF_new,
+               type = 'surface',
+               z = ~volcano )
+      
+      v = plot_ly (OSRI_summary,
+                   type = 'surface',
+                   z = ~volcano ) %>% 
+        layout(
+          xaxis = list(showgrid = FALSE),
+          yaxis = list(showgrid = FALSE))
+      
+      
+    })
+    
+    output$bublePlot <- renderPlotly({
+      library(plotly)
+      
+      vline <- function(x = 0, color = "black") {
+        list(
+          type = "line", 
+          y0 = 0, 
+          y1 = 1, 
+          yref = "paper",
+          x0 = x, 
+          x1 = x, 
+          line = list(color = color)
+        )
+      }
+      
+      hline <- function(y = 0, color = "black") {
+        list(
+          type = "line", 
+          x0 = 0, 
+          x1 = 1, 
+          xref = "paper",
+          y0 = y, 
+          y1 = y, 
+          line = list(color = color)
+        )
+      }
+      
+      # top 20 countries
+      plot_ly(by_country, x = ~mean_masc, y = ~mean_fem, text = ~IP, type = 'scatter', 
+              mode = 'markers', size = ~count, color = ~IP, colors = 'Paired',
+              #Choosing the range of the bubbles' sizes:
+              sizes = c(30, 90),
+              marker = list(opacity = 0.5, sizemode = 'diameter')) %>%
+        layout(xaxis = list(showgrid = T),
+               yaxis = list(showgrid = T),
+               shapes = list(vline(3), hline(3)),
+               showlegend = T)
+    })
+    
+    
+    output$twoDhist <- renderPlotly({
+      count <- with(result, table(Fem_Score, Masc_Score))
+      plot_ly(OSRI_summary, x = ~Masculine, y = ~Feminine, z = ~count) %>%
+        layout(xaxis = list(showgrid = FALSE),
+               yaxis = list(showgrid = FALSE)) %>% 
+        add_histogram2d()
+      
+    })
+    
+    output$pieGender <- renderPlotly({
+      
+      by_gender %>% 
+        plot_ly(labels = ~Gender, values = ~volume) %>%
+        add_pie(hole = 0.6) %>%
+        layout(showlegend = F,
+               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+               showlegend = T) 
+    })
+    
+   output$race <- renderPlot({
+
+     # Androgyny Score by Race
+     
+     osriDF_new$race = factor(osriDF_new$race)
+     ggplot(osriDF_new, aes(x = race, y = Androgyny_Score)) + 
+       geom_boxplot() 
+   })
+   
+   output$radar <- renderPlotly({
+     rc <- plot_ly(
+       type = 'scatterpolar',
+       r = c(91668, 97791, 20159),
+       theta = c('Male', 'Female', 'Other'),
+       fill = 'toself',
+       name = 'Gender'
+     ) %>%
+       layout(
+         polar = list(
+           radialaxis = list(
+             visible = T,
+             range = c(0,120000)
+           )
+         ),
+         showlegend = T
+       )
+     })
+   
+   output$hist2D <- renderPlotly({
+     plot_ly(by_country, x = ~mean_masc, y = ~mean_fem, z = ~mean_andro, 
+             color = ~IP, colors = 'Paired',
+             sizes = c(5, 150),
+             marker = list(symbol = 'circle', opacity = 0.5, sizemode = 'diameter')) %>%
+       add_markers() %>%
+       layout(scene = list(xaxis = list(title = 'Masculinity'),
+                           yaxis = list(title = 'Feminity'),
+                           zaxis = list(title = 'Androgyny Score'),
+                           title = 'Androgyny Result by Country'))
+     
+   })
     
   })
 }
--- ui.R
+++ ui.R
@@ -38,7 +38,7 @@ ui <- dashboardPage(skin = "yellow",
                       mainPanel(
                         
                         tabsetPanel(
-                          tabPanel("Summary",titlePanel("Open Sex Role Inventory Distribution (OSRI)"),
+                          tabPanel("Introduction",titlePanel("Open Sex Role Inventory Distribution (OSRI)"),
                                    fluidRow(
                                      # A static valueBox
                                      valueBox(textOutput("TP"), "Total Participants",width = 3, color = "aqua",icon = icon("users")),
@@ -51,61 +51,96 @@ ui <- dashboardPage(skin = "yellow",
                                      box(width = 12,leafletOutput("CPDMap", height = 350))
                                    )
                                    ,  style='width: 1280px; height: 500px'),
-                          
-                          tabPanel("Analysis",
-                                   
+                          tabPanel("Participant",
                                    fluidRow(
+                                     br(),
                                      box(width = 6,
-                                         title = "Top 20 Country: Feminity vs Masculinity", background = "yellow", solidHeader = TRUE,
-                                         plotlyOutput("bublePlot", height = 350)
-                                       
-                                     ),
+                                     title = "OSRI Participant by Gender", background = "teal", solidHeader = TRUE,
+                                     plotlyOutput("pieGender", height = 400)),
+                                     box(width = 6,
+                                         title = "OSRI Participant by Gender", background = "maroon", solidHeader = TRUE,
+                                         plotlyOutput("radar", height = 400))
+                                     
+                                   )
+                            
+                                   ,  style='width: 1280px; height: 500px'),
+                          tabPanel("Gender Traits",
+                                   fluidRow(
+                                     br(),
+                                     box(width = 6,
+                                       title = "Volcano: Gender by Country", background = "orange", solidHeader = TRUE,
+                                       plotlyOutput("volcanoPlot", height = 400)),
                                      box(
                                        width = 6,
-                                       title = "Volcano: Gender by Country", background = "orange", solidHeader = TRUE,
-                                       plotlyOutput("volcanoPlot", height = 350)
-                                     )
-                                   ),
-                                   
-                                   fluidRow(
-                                     box(width = 3,
-                                         title = "Masculine vs Androgyn",background = "blue", solidHeader = TRUE,
-                                         plotOutput("mascAndro")),
-                                     box(width = 3,
-                                         title = "Feminine vs Androgyny",background = "yellow", solidHeader = TRUE,
-                                         plotOutput("femAndro")),
-                                     box(width = 3,
-                                         title = "Feminine vs Masculine",background = "teal", solidHeader = TRUE,
-                                         plotOutput("femMasc")),
-                                     box(width = 3,
                                          title = "Feminine vs Masculine Traits",background = "olive", solidHeader = TRUE,
-                                         plotlyOutput("twoDhist"))
+                                         plotlyOutput("twoDhist", height = 400)))
                                      
-                                   ),
+                                   ,  style='width: 1280px; height: 500px'),
+                          tabPanel("Country",
                                    fluidRow(
-                                     
+                                     br(),
                                      box(width = 12,
-                                         title = "OSRI Result", background = "maroon", solidHeader = TRUE,
-                                         plotOutput("osriResult", height = 500)
+                                         title = "Top 20 Country: Feminity vs Masculinity", background = "yellow", solidHeader = TRUE,
+                                         plotlyOutput("bublePlot", height = 400)
+                                         
+                                     )
+                                   )
+                                   
+                                   ,  style='width: 1280px; height: 500px'),
+                          tabPanel("Race",
+                                   fluidRow(
+                                     br(),
+                                     box(width = 12,
+                                         title = "Boxplot Androgyny Score by Race", background = "yellow", solidHeader = TRUE,
+                                         plotOutput("race", height = 400)
+                                         
+                                     ),
+                                     fluidRow(
+                                       p("Lagend:1: Mixed race, 2: Asian, 3: Black, 4: Native American, 5: Native Australian, 6: White, 7: Other 0: NA")
+                                       
                                      )
                                      
                                    )
                                    
                                    ,  style='width: 1280px; height: 500px'),
                           
-                          tabPanel("Documentation", titlePanel("User Manual"),
+                          tabPanel("Androgyny Score",
+                                   
+                                    
+                                   fluidRow(
+                                     br(),
+                                     box(width = 12,
+                                         title = "Androgyny Result by Country",background = "yellow", solidHeader = TRUE,
+                                         plotlyOutput("hist2D", height = 400)
+                                   ))
+                                   
+                                   
+                                   
+                                   ,  style='width: 1280px; height: 800px'),
+                          
+                          tabPanel("Documentation", 
                                    
                                    fluidRow(
-                                     h4("Summary Dashboard"),
-                                     br(),
+                                     h5("Introduction Tab"),
                                      p("1.	The dashboard shows the summary of the dataset that consists of total participants, total variables, total country and average elapse time."),
                                      p("2.	The map below shows the distributions of all the participants who took the survey/test."),
-                                     p("3.	Place the cursor at any part of the map to know the details on the test for country of interest."), 
-                                     p("4.	The colour on the map represent the participant density. The darker the colour, the higher number of participants."), 
-                                     p("5.	Black colour represents no participation from that country. "),
-                                     p("6.	User can enlarge the scale of the map by clicking the + and - button on the left side of the map.")
-                                     
-                                     
+                                     p("3.	Place the cursor at any part of the map to know the details on the test for country of interest.  "),
+                                     p("4.	The colour on the map represent the participant density. The darker the colour, the higher number of participants. "),
+                                     p("5.	Black colour represents no participation from that country. User can enlarge the scale of the map by clicking the + and - button on the left side of the map."),
+                                     br(),
+                                     h5("Participant Tab"),
+                                     p(".	Use can view the participant by gender. Blue is male ,Orange is Female, green is no data / NA"),
+                                     br(),
+                                     h5("Gender Traits tab"),
+                                     p(".	The Volcano chart will tell the gender distribution by country.User can move the cursor around the chart and can view different dimension of chart."),
+                                     p(".	Feminine Vs Masculine Traits, user can experience the 2D histogram plot by enlarging the area of interest. The colour scale represents the count of participants and their test score.  "),
+                                     br(),
+                                     h5("Country Tab"),
+                                     p(".	The bubble chart shows the TOP 20 country according to their masculinity and feminity. "),
+                                     br(),
+                                     h5("Race Tab"),
+                                     p(".	The races are rate as:1=Mixed race, 2=Asian, 3=Black, 4=Native American, 5=Native Australian, 6=White, 7=Other 8=NA"),
+                                     p(".	Each boxplot shows the distribution of the race according their test score. ")
                                      
                                    ),
                                    
@@ -113,7 +148,7 @@ ui <- dashboardPage(skin = "yellow",
                                      
                                    )
                                    
-                                   ,  style='width: 1280px; height: 500px')
+                                   ,  style='width: 1280px; height: 1400px')
                         )
                         
                       )
